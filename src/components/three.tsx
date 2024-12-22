25dc9d2a3c36684b26b99ae54f90ccb087493cd3
Prankush Giri <prankushgiri@gmail.com>
2024-12-22 16:21:51 +0530

    removed animations

--- src/components/three.tsx
+++ src/components/three.tsx
@@ -47,7 +47,7 @@ const [maxDistance, setMaxDistance] = useState(
         dampingFactor={0.04}
         makeDefault={true}
         maxDistance={maxDistance}
-        minDistance={20}
+        minDistance={30}
       />
       {/* <pointLight position={[10, 10, 10]} intensity={10} color={"red"} /> */}
       {/* <axesHelper args={[100]} /> */}
